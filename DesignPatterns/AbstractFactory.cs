ee4821c2187d1664ac96c58ae0aedf3216c687a9
Jacob Bohac <Jacob Bohac>
2016-07-02 12:08:30 -0500

    Completed Abstract Factory example

---
+++ DesignPatterns/AbstractFactory.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public abstract class AbstractExhibitFactory
+    {
+        public abstract Predator CreatePredator();
+        public abstract Prey CreatePrey();
+    }
+
+    public class OceanFactory : AbstractExhibitFactory
+    {
+        public override Predator CreatePredator()
+        {
+            return new Shark();
+        }
+
+        public override Prey CreatePrey()
+        {
+            return new Tuna();
+        }
+    }
+
+    public class JungleFactory : AbstractExhibitFactory
+    {
+        public override Predator CreatePredator()
+        {
+            return new Lion();
+        }
+
+        public override Prey CreatePrey()
+        {
+            return new Gazelle();
+        }
+    }
+
+    public abstract class Predator
+    {
+        public abstract void Eat(Prey prey);
+    }
+
+    public abstract class Prey
+    {
+
+    }
+
+    public class Shark : Predator
+    {
+        public override void Eat(Prey prey)
+        {
+            Console.WriteLine(this.GetType().Name + " swam and ate the " + prey.GetType().Name);
+        }
+    }
+
+    public class Lion : Predator
+    {
+        public override void Eat(Prey prey)
+        {
+            Console.WriteLine(this.GetType().Name + " sprinted and ate the " + prey.GetType().Name);
+        }
+    }
+
+    public class Tuna : Prey
+    {
+
+    }
+
+    public class Gazelle : Prey
+    {
+
+    }
+
+    public class Zoo
+    {
+        private Predator _predator;
+        private Prey _prey;
+
+        public Zoo(AbstractExhibitFactory factory)
+        {
+            Console.WriteLine("Created the Zoo!");
+            _predator = factory.CreatePredator();
+            _prey = factory.CreatePrey();
+        }
+
+        public void FeedingTime()
+        {
+            _predator.Eat(_prey);
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -8,8 +8,30 @@ namespace DesignPatterns
 {
     class Program
     {
+        static void AbstractFactoryExample()
+        {
+            /* This example uses the Abstract Factory design pattern to 
+             * create zoo exhibits. The zoo can contain either an ocean
+             * or jungle exhibit depending on the factory passed to the 
+             * zoo constructor. The zoo has a feeding time method available
+             * regardless of the type of exhibit
+             */
+
+            AbstractExhibitFactory oceanFactory = new OceanFactory();
+            AbstractExhibitFactory jungleFactory = new JungleFactory();
+
+            Zoo zoo1 = new Zoo(jungleFactory);
+            zoo1.FeedingTime();
+
+            Zoo zoo2 = new Zoo(oceanFactory);
+            zoo2.FeedingTime();
+
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
+            AbstractFactoryExample();
         }
     }
 }
